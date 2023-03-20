fd96d48d993fe987c22069b563f5c23ad6576d6c
GIOVANA CAMARGO DA SILVA <00001095599252sp@aluno.educacao.sp.gov.br>
2023-03-20 18:25:18 -0300

    meu commit

---
+++ prova1/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+export default function App(){ 
+return(
+<div>
+<Cabecalho/>
+  <Texto/>
+<Cabecalho/>
+</div>
+
+)
+
+} 
+
+
+export function Cabecalho(){
+     return(
+      <div
+style={{backgroundColor:'black',
+      height:150
+}}
+>
+
+      </div>
+     )
+}
+
+
+
+export function Texto(){
+return(
+  <div
+    style={{paddingTop:200,paddingBottom:200}}>
+    <h1>Giovana</h1>
+    </div>
+)
+
+}
